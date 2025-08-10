6b0e9b6ee7f0d59820dfe390633316e5814ac510
Deepanshu Rohilla <20001001027deepanshu@dcrustm.org>
2025-08-10 23:46:23 +0530

    Added user navigation

--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -6,18 +6,31 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion"
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const LandingPage = () => {
+
+  const [longUrl, setLongUrl] = useState();
+  const navigate = useNavigate()
+
+  const handleShorten = (e) => {
+    e.preventDefault()
+    if(longUrl) navigate(`/auth?createNew=${longUrl}`);
+  }
+
   return (
     <div className="flex flex-col items-center">
       <h2 className="my-10 sm:my-16 text-3xl sm:text-6xl lg:text-7xl  text-white text-center font-extrabold">
         The only URL Shortener <br /> you'll ever need! 👇
       </h2>
 
-      <form className="sm: h-14  flex flex-col sm:flex-row w-full md:w-2/4 gap-2">
+      <form onSubmit={handleShorten} className="sm: h-14  flex flex-col sm:flex-row w-full md:w-2/4 gap-2">
         <Input
         type="url"
+        value={longUrl}
         placeholder="Enter your loooong URL"
+        onChange={(e) => setLongUrl(e.target.value)}
         className="h-full flex-1 py-4 px-4"
          />
         <Button className="h-full" type="submit" variant="destructive">
