9de41b4bb08a3ab1c9776718b0e1bc2a2f764c95
Deepanshu Rohilla <20001001027deepanshu@dcrustm.org>
2025-08-12 00:26:17 +0530

    Added the login auth

--- src/pages/auth.jsx
+++ src/pages/auth.jsx
@@ -1,5 +1,7 @@
 import { useSearchParams } from "react-router-dom"
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import Login from "@/components/login"
+import Signup from "@/components/signup"
 
 const Auth = () => {
 
@@ -15,8 +17,12 @@ const Auth = () => {
         <TabsTrigger value="login">Login</TabsTrigger>
         <TabsTrigger value="signup">Signup</TabsTrigger>
       </TabsList>
-      <TabsContent value="login">Login Component</TabsContent>
-      <TabsContent value="signup">Signup Component</TabsContent>
+      <TabsContent value="login">
+        <Login />
+      </TabsContent>
+      <TabsContent value="signup">
+        <Signup />
+      </TabsContent>
     </Tabs>
     </div>
   )
