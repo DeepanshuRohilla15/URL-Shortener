c54c84c70bb5bf7cc71056fb63dccb7745e0b7f8
Deepanshu Rohilla <20001001027deepanshu@dcrustm.org>
2025-08-15 23:47:50 +0530

    Creating a singup API

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Dashboard from "./pages/dashboard"
 import Auth from "./pages/auth"
 import Link from "./pages/link"
 import RedirectLink from "./pages/redirect-link"
+import UrlProvider from "./context"
 
 
 const router = createBrowserRouter([
@@ -36,7 +37,12 @@ const router = createBrowserRouter([
 ])
 
 function App() {
-  return <RouterProvider router={router} />
+  return (
+    <UrlProvider>
+      <RouterProvider router={router} />
+    </UrlProvider>
+  )
+   
 }
 
 export default App
--- src/components/login.jsx
+++ src/components/login.jsx
@@ -16,6 +16,7 @@ import * as Yup from "yup"
 import { login } from "@/db/apiAuth"
 import useFetch from "./hooks/useFetch"
 import { useNavigate, useSearchParams } from "react-router-dom"
+import { UrlState } from "@/context"
 
 const Login = () => {
     const [errors, setErrors] = useState([])
@@ -37,12 +38,14 @@ const Login = () => {
         }));
     };
 
-    const {data, error, loading, fn: fnLogin} = useFetch(login, formData)
+    const {data, error, loading, fn: fnLogin} = useFetch(login, formData);
+    const {fetchUser} = UrlState()
 
     useEffect(() => {
         if(error === null && data)
         {
             navigate(`/dashboard?${longLink ? `createNew=${longLink}`: ""}`)
+            fetchUser();
         }
     }, [data, error])
 
